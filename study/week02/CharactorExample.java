a40865d38f5263919c4c31cc74be106a1359cbfc
HyungjuJang <dacapolife87@gmail.com>
2020-11-20 10:08:19 +0900

    2주차 - 프리미티브타입

---
+++ study/week02/CharactorExample.java
@@ -0,0 +1,15 @@
+package week02;
+
+public class CharactorExample {
+
+    public static void main(String[] args) {
+
+        char charUseU0000 = '\u0000';
+
+        System.out.println("charUseU0000 = " + charUseU0000);
+
+//        char charUseNull = null;
+//
+//        System.out.println("charUseNull = " + charUseNull);
+    }
+}
