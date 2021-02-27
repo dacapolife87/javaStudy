8bb7dc856109914f5dce132a941f3d906ffb14af
HyungjuJang <dacapolife87@gmail.com>
2021-02-27 22:36:48 +0900

    feat : Exception Case 샘플 예제

--- study/week09/src/ExceptionExample.java
+++ study/week09/src/ExceptionExample.java
@@ -7,14 +7,14 @@ public class ExceptionExample {
     public static void main(String[] args) {
         ExceptionExample example = new ExceptionExample();
 
-
-        example.customException();
-
-        try {
-            example.customException2();
-        } catch (Exception e) {
-            System.out.println("예외처리");
-        }
+        example.tryCatch();
+//        example.customException();
+//
+//        try {
+//            example.customException2();
+//        } catch (Exception e) {
+//            System.out.println("예외처리");
+//        }
 
 
     }
@@ -38,5 +38,21 @@ public class ExceptionExample {
             throw new CustomException("잘못된 이름");
         }
 
+    }
+
+    public void tryCatch() {
+        try {
+            System.out.println("Try1");
+            try {
+                System.out.println("InnerTry");
+                throw new Exception("InnerException");
+            }catch (Exception e) {
+                System.out.println("Inner Catch!");
+            }
+        } catch (Exception e) {
+            System.out.println("OuterCatch");
+        } finally {
+            System.out.println("Final!");
+        }
     }
 }
